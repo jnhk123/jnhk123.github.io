901cb3ed0da07b0352db4da9326767f4dbb3b815
junhyuk <49514381+jnhk123@users.noreply.github.com>
2022-11-12 00:44:46 +0900

    날짜 표기 수정

--- src/mixins/date.js
+++ src/mixins/date.js
@@ -2,7 +2,7 @@ export default {
   methods: {
     dateToYmd(date) {
       const year = String(date.getFullYear()).padStart(2, 0);
-      const month = String(date.getMonth()).padStart(2, 0);
+      const month = String(date.getMonth() + 1).padStart(2, 0);
       const day = String(date.getDate()).padStart(2, 0);
       return `${year}-${month}-${day}`;
     },
